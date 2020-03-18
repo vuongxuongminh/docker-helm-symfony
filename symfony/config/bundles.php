39323007d7c3e4673ac92a8791030ba6250f9cb4
Minh Vuong <vuongxuongminh@gmail.com>
2020-03-18 16:59:46 +0700

    Add ORM pack.

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
