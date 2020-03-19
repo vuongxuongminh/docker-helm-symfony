f7aeaf841d66843b290413fbf7043bc6222cfc56
Minh Vuong <vuongxuongminh@gmail.com>
2020-03-19 22:47:28 +0700

    Add `setup` Docker service support setup project.

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'fpm' ] || [ "$1" = 'supervisor' ]; then
+if [ "$1" = 'fpm' ] || [ "$1" = 'supervisor' ] || [ "$1" = 'init' ]; then
   mkdir -p /app/var/cache /app/var/log
+
   setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX /app/var
   setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX /app/var
-
-  if [ "$APP_ENV" = 'dev' && "$1" = 'fpm' ]; then
-    # Install composer package & run migrate on dev env. It should run once on `fpm` or `supervisor` service.
-
-		composer install --prefer-dist --no-progress --no-suggest --no-interaction
-
-    echo "Waiting for db to be ready..."
-
-    until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
-      sleep 1
-    done
-
-    if ls -A src/Migrations/*.php > /dev/null 2>&1; then
-      bin/console doctrine:migrations:migrate --no-interaction
-    fi
-	fi
 
 	if [ "$1" = 'supervisor' ]; then
 	  cp /var/supervisord/base.conf /var/supervisord/supervisord.conf
@@ -38,6 +23,22 @@ if [ "$1" = 'fpm' ] || [ "$1" = 'supervisor' ]; then
     setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX /var/supervisord
   elif [ "$1" = 'fpm' ]; then
     set -- php-fpm
+  elif [ "$1" = 'setup' ]; then
+    # Install composer package & run migrate on dev env.
+
+    composer install --prefer-dist --no-progress --no-suggest --no-interaction
+
+    echo "Waiting for db to be ready..."
+
+    until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
+      sleep 1
+    done
+
+    if ls -A src/Migrations/*.php > /dev/null 2>&1; then
+      bin/console doctrine:migrations:migrate --no-interaction
+    fi
+
+    exit 0
 	fi
 
 fi
