9ab4cdf9286042b44864e0166dae029501a57951
Minh Vuong <vuongxuongminh@gmail.com>
2021-04-29 00:18:37 +0700

    WIP

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,42 +1,28 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'fpm' ] || [ "$1" = 'supervisor' ] || [ "$1" = 'setup' ]; then
+if [ "$1" = 'fpm' ] || [ "$1" = 'setup' ]; then
   mkdir -p /symfony/var/cache /symfony/var/log
 
   setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX /symfony/var
   setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX /symfony/var
 
-	if [ "$1" = 'supervisor' ]; then
-	  cp /var/supervisord/base.conf /var/supervisord/supervisord.conf
 
-    { \
-      echo '[inet_http_server]'; \
-      echo 'port = *:9000'; \
-      echo "username = ${SUPERVISOR_USERNAME:-root}"; \
-      echo "password = ${SUPERVISOR_PASSWORD:-root}"; \
-    } >> /var/supervisord/supervisord.conf
-
-    set -- supervisord -c /var/supervisord/supervisord.conf
-
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX /var/supervisord
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX /var/supervisord
-  elif [ "$1" = 'fpm' ]; then
+  if [ "$1" = 'fpm' ]; then
     set -- php-fpm
   elif [ "$1" = 'setup' ]; then
-    # Install composer package & run migrate on dev env.
+    umask 0000
 
-    composer install --prefer-dist --no-progress --no-suggest --no-interaction
-
-    echo "Waiting for db to be ready..."
-
-    until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
-      sleep 1
-    done
-
-    if ls -A src/Migrations/*.php > /dev/null 2>&1; then
-      bin/console doctrine:migrations:migrate --no-interaction
+    # First time we need to create project
+    if [ ! -f composer.json ]; then
+      composer create-project symfony/skeleton tmp/ --prefer-dist --no-dev --no-progress --no-interaction;
+      cp -Rp tmp/. .
+      rm -Rf tmp/
     fi
+
+    # Next time just ensure dependencies are up to date.
+
+    composer install --prefer-dist --no-progress --no-interaction
 
     exit 0
 	fi
